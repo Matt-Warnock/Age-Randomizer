6052567c7fc5f8d1f5fdccf554cdc595d30f7001
MattW <drflamerock@yahoo.co.uk>
2020-02-16 17:47:39 +0000

    added console.log change of page title

--- randomizer.js
+++ randomizer.js
@@ -2,3 +2,4 @@ var myElement = document.getElementById('age');
 var myRandomNumber = Math.floor(Math.random() * 100);
 var mySentence = `I am ${myRandomNumber} years old.`;
 myElement.innerHTML = mySentence;
+console.log(mySentence);
