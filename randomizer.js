2c6a979375bf248b525aae2a5046ca62ff2f45e3
MattW <drflamerock@yahoo.co.uk>
2020-02-19 11:21:02 +0000

    myButton targeted, eventlistener added

--- randomizer.js
+++ randomizer.js
@@ -3,3 +3,7 @@ var myRandomNumber = Math.floor(Math.random() * 100);
 var mySentence = `I am ${myRandomNumber} years old.`;
 myElement.innerHTML = mySentence;
 console.log(mySentence);
+var myButton = document.getElementsByName('myButton');
+myButton.addEventlistener('click', event =>{
+  
+});
