351c333368f721e63e8651f671adbb3ef4f11e10
MattW <drflamerock@yahoo.co.uk>
2020-02-19 16:06:53 +0000

    bug fix on js

--- randomizer.js
+++ randomizer.js
@@ -3,13 +3,13 @@ var myRandomNumber = Math.floor(Math.random() * 100);
 var mySentence = `I am ${myRandomNumber} years old.`;
 myElement.innerHTML = mySentence;
 console.log(mySentence);
-var myButton = document.getElementsByid('myButton');
+var myButton = document.getElementById('myButton');
 myButton.addEventListener('click', function(event){
   var myNoun = document.getElementById('noun').value;
   var myVerb = document.getElementById('verb').value;
   var myAdject = document.getElementById('adject').value;
   var myAdverb = document.getElementById('adverb').value;
-  var myMadLab = `Do you ${myVerb} your ${myAdject} ${myNoun} ${myAdverb}?<br>Thats hilarious!!`;
+  var myMadLab = `Do you ${myVerb} your ${myAdject} ${myNoun} ${myAdverb}`;
   var myTarget = document.getElementById('target');
   myTarget.innerHTML = myMadLab;
 });
