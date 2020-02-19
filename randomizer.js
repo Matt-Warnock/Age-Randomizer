1a6157e5fd4062c1f6410fc388f62007b95efebb
MattW <drflamerock@yahoo.co.uk>
2020-02-19 11:42:26 +0000

    madlab object and target object created

--- randomizer.js
+++ randomizer.js
@@ -9,5 +9,7 @@ myButton.addEventlistener('click', event =>{
   var myVerb = document.getElementById('verb').value;
   var myAdject = document.getElementById('adject').value;
   var myAdverb = document.getElementById('adverb').value;
+  var myMadLab = `Do you ${myVerb} your ${myAdject} ${myNoun} ${myAdverb}?<br>Thats hilarious!!`;
+  var myTarget = document.getElementById('target');
   
 });
